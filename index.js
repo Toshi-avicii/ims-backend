f28b726c92ea1e5e824ef5fb530deed8cbd25bc5
Tushar <tushar.toshi12@gmail.com>
2022-11-15 19:42:15 +0530

    Third commit

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ const counselorsTrashRoute = require('./routes/counselorsTrashRoute')
 // setting time zone
 process.env.TZ = env.TZ;
 // db connection 
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('client/build'));
-
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-}
 
 connect();
 app.use(cors());
